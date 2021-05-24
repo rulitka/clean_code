6204dff29985137401523a7a93c8372b3867ce61
rulitka <59177208+rulitka@users.noreply.github.com>
2021-05-24 19:16:53 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,35 @@
+new_matrix – arrayDoorsBool (array_Doors_Bool) 
+// массив дверей - логических элементов
 
+convert_array - arrayDoorsInt (array_Doors_Integer)
+// массив дверей – чисел
+
+result – resultDoorsString (result_Doors_String)
+// итоговое значение дверей - строка
+
+n – arrayForTurn (array_For_Turn)
+// массив для разворота элементов, расположенных по убыванию 
+
+length – lengthArrayF (length_Array_F)
+//длина массива F
+
+f_sort – sortedArrayF (sorted_Array_F)
+// Отсортированный массив F
+
+tmp – currElemArrayForTurn (curren_Element_Array_For_Turn)
+// текущий элемент массива для разворота элементов, расположенных по убыванию
+
+n – MatrixRotationCounter (Matrix_Rotation_Counter)
+// счетчик поворотов матрицы
+
+mat – MatrixFromString (Matrix_From_String)
+// преобразование строки в матрицу
+
+сount – dictLettersRepeat (dictionary_Letters_Repeat)
+// словарь букв и их повторений
+
+new_values – arrayDictValues (array_Dictionary_Values)
+//массив, в котором хранятся значения словаря
+
+Out – stringDeletedDuplicateLetters (string_Deleted_Duplicate_Letters)
+//строка, в которой удалены все повторяющиеся буквы
