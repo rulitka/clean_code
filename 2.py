21c47f88a86c75533afe0fe8c95415228c3b1390
rulitka <59177208+rulitka@users.noreply.github.com>
2021-05-27 19:30:20 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,32 @@
 6.1
 //учтены уровни абстракции
+1 new_values – QuantityOfLetters
+2 new_matrix - CurrentNumberOfDoors
+3 new_matrix - CurrentStateOfTree
+4 count - LettersAndTheitQuantity
+5 temp_matrix - ChangeThreeDigits
+
 6.2
-//использованы технические термины из информатики
+//использованы технические термины из информатики, математики, паттерны, алгоритмы
+1 new_board - clipboard 
+2 sq_2  - squareMultipliesItself
+3 square - numbersDividedInHalf
+4 sq_new - squareOfNAndM
+
 6.3
-//имена переменных даны с учётом контекста (функции, метода, класса).
+//имена переменных даны с учётом контекста (функции, метода, класса)
+1 leftBoard, leftUser, rightBoard, rightUser
+2 userFirstName, userLastName, userAdress
+3 Переменные leftBranch, rightBranch, centralBranch вместо этого создать класс classTree и сделать эти переменные его атрибутами.
+class Tree():
+    leftBranch = 1
+    rightBranch = 2
+    centralBranch = 3
+
 6.4
 //имена переменных не укладываются в 8-20 символов
+1 stringDeletedDuplicateLetters – strDelDoubleLetters
+2 time - motoTimeHours
+3 day - dayOfConquestCampain
+4 sum_all - summOfAllElements
+5 summ - summOfTwoNeighElement
