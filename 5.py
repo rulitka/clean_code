02a7aa454200a89692fb8d469cd3f7ccb0d1e3d7
rulitka <59177208+rulitka@users.noreply.github.com>
2021-06-06 18:34:34 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,28 @@
+//Так ка у меня нет задач, решенных  помощью классов, 
+//ниже представлены названия классов, методов и объектов, 
+//какими бы они могли быть, если бы я решала задаи с использованием ООП.
+
+1. class TrafficLight(object), class Road(object), 
+// 14
+2. class Motorcycle(object)
+// 2
+3. Class Field(object), Class Game(object), Class Position(object)
+// 3
+4. class TextEditor(object)
+// 20
+5. Class Tree(object), Class Branch(object) 
+// 23
+
+
+1. def set_color(), def get_color(), def get_distance()
+// 14
+2. def get_distance()
+// 2
+3. def get_position()
+// 3
+4.def add_symbol(), def delete_symbol(), def undo_operation(), def redo_operation()
+// 20
+5. def add_branch(), def delete_btanch()
+// 23
+6.  
+7.
