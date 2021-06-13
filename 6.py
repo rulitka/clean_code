79ad53118a9f04ed531d2fbf0c1e263b019ec340
rulitka <59177208+rulitka@users.noreply.github.com>
2021-06-13 16:11:57 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,25 @@
+1.	def count(width, s, subs) –  def DivideStringByWidth(width, s, subs)
+// функция получает на вход строку и делит ее на отрезки по заданной ширине  - задача 7 
+2.	def MaximumDiscount(N, price) - def CheckSizerOfN (N, price)
+//функия проверяет размер N - задача 16 
+3.	def get_max_discount(new_massive) -  def FindMaxDiscount(new_massive)
+//функция находит размер максимальной скидки - задача 16
+4.	def create_matrix(N, price) -  def DivideNIntoThree(N, price)
+// функция делит полученное ранее N  по три элемента - задача 16
+5.	def chech_first_last(line) - def IsAsteriskFirstAndLastElement(line)
+// функция проверяет является ли первый и последний элемент звездочкой-  задача 17
+6.	def check_list(N, data) - def CheckElementsIfNotOrder(N, data)
+// функция проверяет возрастают ли элементы в списке  - задача 18
+7.	def check_matrix_one(new_data) – CheckAndChangeOrderList(new_data)
+// функция проверяем возрастают ли элементы в списке и если не возрастают, то меняет их местами - задача 18
+8.	def check_similar(in_put) – def SearchIdentLettersInString (in_put)
+// функция ищет одинаковые буквы в строке  - задача 21
+9.	def rotateChar(in_put) – def SwapLettersInString(in_put);
+// функция меняет местами буквы в строке  - задача 21
+10.	def rotateSyl(temp_matrix) - def RotateElementsOfMatrix(temp_matrix)
+// функция меняет местами элементы в матрице  - задача 21
+11.	def divideSyl(in_put) - def DivideStringIntoSyllables(in_put)
+// функция делит строку на слоги  - задача 21
+12.	def oddswap(in_put) – def AddAllPermutToMatrix(in_put)
+// функция добавляет все варианты сочетания букв в матрицу  - задача 21
 
